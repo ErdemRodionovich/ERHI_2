e08e870402293f5e9ac1f94d716e1d5358865fe6
erdem_badluev <erdembad@rambler.ru>
2021-05-17 08:27:35 +0300

    Create menu panel.
    Продолжил делать меню.

---
+++ Assets/Scripts/UI_Manager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UI_Manager : MonoBehaviour
+{
+
+    [SerializeField] private GameObject menu;
+    [SerializeField] private TextMeshProUGUI countText;
+    [SerializeField] private TextMeshProUGUI cicleText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnMenuButtonClicked()
+    {
+        menu.SetActive(true);
+    }
+
+}
